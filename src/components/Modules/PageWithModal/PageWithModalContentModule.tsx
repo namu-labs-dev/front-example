fc0d62ed86c6fa6c8f535c57584cbe39120c1350
Daewoo Koh <bdkfasd@naver.com>
2024-06-07 09:57:56 +0900

    etc

--- src/components/Modules/PageWithModal/PageWithModalContentModule.tsx
+++ src/components/Modules/PageWithModal/PageWithModalContentModule.tsx
@@ -8,9 +8,30 @@ export const PageWithModalContentModule = (props: Props) => {
   return (
     <div className="m-[20px]">
       <div className="text-[20px]">{props.title}</div>
-      <Button className="my-3" type="primary">
-        Open Modal
-      </Button>
+
+      <div>
+        <Button className="my-3" type="primary">
+          Open Modal
+        </Button>
+      </div>
+
+      <div>
+        <Button className="my-3" type="primary">
+          Open Drawer
+        </Button>
+      </div>
+
+      <div>
+        <Button className="my-3" type="primary">
+          Open DrawerPage
+        </Button>
+      </div>
+
+      <div>
+        <Button className="my-3" type="primary">
+          Open Toast
+        </Button>
+      </div>
     </div>
   );
 };
