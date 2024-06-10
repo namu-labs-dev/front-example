fd7d5ba37a5a2704796dbeff1a202663cca3939d
bsangs <tkdals6805@naver.com>
2024-06-10 11:35:23 +0900

    feat: atom

--- src/app/bsangs/workbench/page.tsx
+++ src/app/bsangs/workbench/page.tsx
@@ -1,18 +1,24 @@
 "use client";
 
+import { useState } from "react";
 import { BsangsFooterAtom } from "~/components/Atoms/BsangsFooterAtom/BsangsFooterAtom";
 import { BsangsHeaderAtom } from "~/components/Atoms/BsangsHeaderAtom/BsangsHeaderAtom";
 
 export default function Bsangs() {
+  const [selectedButton, setSelectedButton] = useState<string>("home");
   return (
     <div>
       <BsangsHeaderAtom
         onClickLeftIcon={() => alert("Connect")}
-        onClickRightIcon={() => console.log("Setting")}
+        onClickRightIcon={() => alert("Setting")}
         title="Proxima OS"
       />
 
-      <BsangsFooterAtom title="Good" />
+      <BsangsFooterAtom
+        title="Good"
+        selectedButton={selectedButton}
+        onSelect={(buttonName) => setSelectedButton(buttonName)}
+      />
     </div>
   );
 }
--- src/components/Atoms/BsangsFooterAtom/BsangsFooterAtom.tsx
+++ src/components/Atoms/BsangsFooterAtom/BsangsFooterAtom.tsx
@@ -2,16 +2,63 @@ import SVGAtom from "../SVGAtom/SVGAtom";
 
 type Props = {
   title: string;
+  selectedButton?: string;
+  onSelect?: (buttonName: string) => void;
+};
+
+const FooterMenu = (props: {
+  iconName: string;
+  name: string;
+  selectedButton?: string;
+  onSelect?: (buttonName: string) => void;
+  children?: React.ReactNode;
+}) => {
+  const { iconName, name, selectedButton, onSelect } = props;
+
+  const isSelected = selectedButton === name;
+
+  let cn = "flex h-full w-full cursor-pointer items-center justify-center";
+
+  if (isSelected) {
+    cn += " box-border border-t-2 border-[#00EC97]";
+  }
+
+  return (
+    <div className={cn} onClick={() => onSelect && onSelect(name)}>
+      <SVGAtom
+        iconName={iconName as any}
+        width={20}
+        height={20}
+        color={isSelected ? "white" : "#8C8C8C"}
+        className="overflow-visible"
+      />
+    </div>
+  );
 };
 
 export const BsangsFooterAtom = (props: Props) => {
+  const { selectedButton, onSelect } = props;
+
+  const buttons = [
+    { iconName: "dollarCircle", name: "home" },
+    { iconName: "message", name: "message" },
+    { iconName: "wallet", name: "wallet" },
+    { iconName: "history", name: "history" },
+  ];
+
   return (
-    <div className="flex h-full items-center justify-center bg-[#FFC96F]">
-      <SVGAtom iconName="dollarCircle" />
-      <SVGAtom iconName="message" />
-      <SVGAtom iconName="wallet" />
-      <SVGAtom iconName="history" />
-      <div>{props.title}</div>
+    <div className="flex h-[60px] items-center justify-between bg-black">
+      {buttons.map((button) => {
+        return (
+          <FooterMenu
+            key={button.name}
+            iconName={button.iconName}
+            name={button.name}
+            selectedButton={selectedButton}
+            onSelect={onSelect}
+          />
+        );
+      })}
     </div>
   );
 };
