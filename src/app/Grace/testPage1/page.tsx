9c334bda5eef66fe4651aebc6c2a00a9bef09519
Gracified2004 <izeerhunmwunsee@gmail.com>
2024-06-15 11:38:39 +0100

    Add testPage1 route

---
+++ src/app/Grace/testPage1/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TestPage1 = () => {
+  return (
+    <div>
+      <h1>Test Page 1</h1>
+      <p>This is  my Test Page 1.</p>
+    </div>
+  );
+};
+
+export default TestPage1;
