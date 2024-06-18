2554938f0832f0e9a1b89387ff14c79bd8490d3e
Spacedaway <ekanabasiisok@gmail.com>
2024-06-18 21:43:31 +0100

    feat: add vote container

---
+++ src/app/ekan/testPage2/vote/container/EkanVoteContainer.tsx
@@ -0,0 +1,40 @@
+import { EkanVoteTemplate } from "~/components/Templates/EkanVote/EkanVoteTemplate";
+
+export const EkanVoteContainer = () => {
+  const homeTemplateProps: React.ComponentProps<typeof EkanVoteTemplate> = {
+    headerModuleProps: {
+      title: "Vote",
+    },
+    voteBannerProps: {
+      deductionAmount: 9,
+      votingAmount: 10,
+    },
+    formContentModuleProps: {
+      formCategory: {
+        label: "Category",
+        placeholder: "Birthday AD",
+        name: "category",
+      },
+      formTitle: {
+        label: "Vote Title",
+        name: "title",
+        placeholder: "Title",
+      },
+      formImageUpload: {
+        label: "Image",
+        name: "image",
+      },
+      formDescription: {
+        label: "Description",
+        name: "description",
+        placeholder: "Please write down a description of the event",
+      },
+      formDate: {
+        label: "Start",
+        name: "date",
+      },
+    },
+  };
+
+  return <EkanVoteTemplate {...homeTemplateProps} />;
+};
