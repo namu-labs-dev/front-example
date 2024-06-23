c5b05581c4307eb2cc2e6e4c783c9264f5bd0cd6
MikeRock51 <mikerockmusic51@gmail.com>
2024-06-23 14:12:41 +0100

    fix: nvir svg scaling issue

--- src/components/Components/MessageCard/MessageCard.tsx
+++ src/components/Components/MessageCard/MessageCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 import SVGAtom from "~/components/Atoms/SVGAtom/SVGAtom";
 import { type messageProps } from "~/components/Modules/TestPage1/TestPage1ContentModule";
@@ -9,12 +10,22 @@ type Prop = {
 export default function MessageCard(props: Prop) {
   return (
     <div>
-      <div className='flex items-center p-3'>
+      <div className='flex items-center gap-2 p-3'>
         <SVGAtom
           iconName={props.message.from.toLowerCase() as "eth" | "nvir"}
           width={44}
           height={44}
         />
+        <div className='w-full'>
+          <div className='flex justify-between'>
+            <p className='font-bold'>{props.message.from}</p>
+            <p className='text-sm'>{props.message.recievedAt}</p>
+          </div>
+          <div className='flex justify-between'>
+            <p className='text-sm font-semibold'>{props.message.message}</p>
+            <p className='text-sm'>{props.message.recievedAt}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
